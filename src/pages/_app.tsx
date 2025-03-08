1626904ef0101a963f6fc4e7169c2b1ce777e90d
nicx519y <nicx519y@gmail.com>
2025-03-09 02:35:02 +0800

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import Navigation from '@/components/Navigation';
 import Footer from '@/components/Footer';
 import '@/styles/globals.css';
+import Head from 'next/head';
 
 // 不需要通用布局的页面路径
 const noLayoutPages = ['/login', '/register', '/forgot-password', '/404'];
@@ -20,16 +21,22 @@ export default function App({ Component, pageProps, router }: AppProps) {
   }
 
   return (
-    <div className="bg-[#e5e5e5] min-h-screen">
-      <div className="w-[1024px] mx-auto">
-        <Navigation />
-        <div className="h-[16px]"></div>
-        <main className="flex-1">
-          <Component {...pageProps} />
-        </main>
-        <div className="h-[16px]"></div>
-        <Footer />
+    <>
+      <Head>
+        <title>悦己—悦健康，悦自在</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-[#e5e5e5] min-h-screen">
+        <div className="w-[1024px] mx-auto">
+          <Navigation />
+          <div className="h-[16px]"></div>
+          <main className="flex-1">
+            <Component {...pageProps} />
+          </main>
+          <div className="h-[16px]"></div>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 } 
